303be5bef7040c6e79c5efdbc196c1ea604b8f31
Colin Bakker <acojaba@gmail.com>
2024-01-28 17:52:02 -0600

    Removed stat placeholder

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 let last_time = null;
 let total_time = 0;
 
-let stat = 0;
 let population = 10000;
 let unemployed = population;
-const dstat = 0.001;
-let statBuildings = 0;
-
 
 const popText = document.querySelector("#popText");
-const statButton = document.querySelector("#statButton");
-const statBuildingButton = document.querySelector("#statBuildingButton");
-const statText = document.querySelector("#statText");
-const statBuildingText = document.querySelector("#statBuildingText");
-
-statButton.onclick = statButtonClick;
-statBuildingButton.onclick = statBuildingButtonClick;
-
-
-function statButtonClick() {
-	stat++;
-}
-
-function statBuildingButtonClick() {
-	if(stat >= 10){
-		stat -= 10;
-		statBuildings++;
-	}
-}
-
 
 setInterval(function gameLoop() {
 	const current_time = performance.now();
@@ -43,17 +19,10 @@ setInterval(function gameLoop() {
 }, 1000/60);
 
 function updateGame(delta_time, total_time) {
-	//./resources.js funcitons
 	updateResourceValues(delta_time, total_time);
 	updateResourceText();
 	updateBuildingText();
-
-
-	stat += (dstat * statBuildings) * delta_time;
-	statText.innerText = stat.toFixed(2); //only shows 2 decimal places
-	statBuildingText.innerText = statBuildings;
 	
 	popText.innerText = population;
-
 }
 
