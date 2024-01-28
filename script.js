17100b0ec6f496889e29cb6c640218e9cf0b1f47
Colin Bakker <acojaba@gmail.com>
2024-01-28 15:37:01 -0600

    Added interval loop
    Created a loop using set interval and tested out increasing stat over time

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
+let last_time = null;
+let total_time = 0;
+
 let stat = 0;
+const dstat = 0.001;
+let statBuildings = 0;
 
-const button1 = document.querySelector("#button1");
+const statButton = document.querySelector("#statButton");
+const statBuildingButton = document.querySelector("#statBuildingButton");
+const statText = document.querySelector("#statText");
+const statBuildingText = document.querySelector("#statBuildingText");
 
-const statText = document.querySelector("#stat1Text");
+statButton.onclick = statButtonClick;
+statBuildingButton.onclick = statBuildingButtonClick;
+function statButtonClick() {
+	stat++;
+}
 
-button1.onclick = button1click;
+function statBuildingButtonClick() {
+	if(stat >= 10){
+		stat -= 10;
+		statBuildings++;
+	}
+}
 
-function button1click() {
-	stat += 1;
-	console.log(stat);
-	statText.innerText = stat;
+
+setInterval(function gameLoop() {
+	const current_time = performance.now();
+	if(last_time === null) {
+		last_time = current_time;
+	}
+	const delta_time = current_time - last_time;
+	total_time += delta_time;
+	last_time = current_time;
+
+	updateGame(delta_time, total_time);
+}, 1000/60);
+
+function updateGame(delta_time, total_time) {
+	stat += (dstat * statBuildings) * delta_time;
+	statText.innerText = stat.toFixed(2); //only shows 2 decimal places
+	statBuildingText.innerText = statBuildings;
 }
